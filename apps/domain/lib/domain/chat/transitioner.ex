364ad82d198bed63f4d7b5f38acd2a16c2b38b41
mpraski <marcin.praski@live.com>
2020-05-17 21:32:40 +0200

    Simplify transitioner logic

--- apps/domain/lib/domain/chat/transitioner.ex
+++ apps/domain/lib/domain/chat/transitioner.ex
@@ -3,10 +3,15 @@ defmodule Chat.Transitioner do
 
   @initial_scenario "initial"
   @terminal_scenario "terminal"
+  @defaults %{
+    next_question: nil,
+    next_scenario: nil,
+    new_scenario: nil,
+    new_scenarios: nil,
+    load_scenarios: nil
+  }
 
-  def transition({scenarios, question, data}, {:single, answer}) do
-    [current | _] = scenarios
-
+  def transition({[current | _], question, _} = context, {:single, answer}) do
     %Question.Single{
       answers: answers
     } = Chat.question(current, question)
@@ -18,22 +23,17 @@ defmodule Chat.Transitioner do
       comments: comments
     } = answers |> Enum.find(nil, &(&1.id == answer))
 
-    {scenarios, question} =
-      next(
-        next_question,
-        next_scenario,
-        new_scenario,
-        scenarios
-      )
-
-    data = data |> prepare_comments(comments, current)
-
-    {scenarios, question, data} |> load_messages()
+    context
+    |> core(
+      next_question: next_question,
+      next_scenario: next_scenario,
+      new_scenario: new_scenario
+    )
+    |> put_comments(comments, current)
+    |> load_messages()
   end
 
-  def transition({scenarios, question, data}, {:multiple, answers}) do
-    [current | _] = scenarios
-
+  def transition({[current | _], question, _} = context, {:multiple, answers}) do
     %Question.Multiple{
       decisions: decisions,
       load_scenarios: load_scenarios
@@ -46,57 +46,43 @@ defmodule Chat.Transitioner do
       comments: comments
     } = decisions |> find_decision(answers)
 
-    {scenarios, question} =
-      next(
-        next_question,
-        next_scenario,
-        new_scenario,
-        scenarios
-      )
-
-    scenarios =
-      if load_scenarios do
-        answers |> Enum.reduce(scenarios, &load_scenario(&2, &1))
-      else
-        scenarios
-      end
-
-    data = data |> prepare_comments(comments, current)
-
-    {scenarios, question, data} |> load_messages()
+    context
+    |> core(
+      next_question: next_question,
+      next_scenario: next_scenario,
+      new_scenario: new_scenario,
+      new_scenarios: answers,
+      load_scenarios: load_scenarios
+    )
+    |> put_comments(comments, current)
+    |> load_messages()
   end
 
-  def transition({scenarios, question, data}, {:prompt, answer}) do
-    [current | _] = scenarios
-
+  def transition({[current | _], question, _} = context, {:prompt, answer}) do
     %Question.Prompt{
-      id: id,
       leads_to: next_question,
       jumps_to: next_scenario,
       loads_scenario: new_scenario,
       comments: comments
     } = Chat.question(current, question)
 
-    {scenarios, question} =
-      next(
-        next_question,
-        next_scenario,
-        new_scenario,
-        scenarios
-      )
-
-    data =
-      data
-      |> prepare_comments(comments, current)
-      |> Map.put(id, answer)
-
-    {scenarios, question, data} |> load_messages()
+    context
+    |> core(
+      next_question: next_question,
+      next_scenario: next_scenario,
+      new_scenario: new_scenario
+    )
+    |> put_comments(comments, current)
+    |> put_free_text(answer)
+    |> load_messages()
   end
 
   def transition do
     %Scenario{start: start} = Chat.scenario(@initial_scenario)
-    {[@initial_scenario], start, %{}}
+    {[@initial_scenario], start, %{}} |> load_messages()
   end
+
+  def load_messages({[], _, _} = context), do: context
 
   def load_messages({scenarios, question, data} = context) do
     [current | _] = scenarios
@@ -116,23 +102,25 @@ defmodule Chat.Transitioner do
     end
   end
 
-  defp next(next_question, next_scenario, new_scenario, scenarios) do
+  defp core({scenarios, _, data}, opts \\ []) do
+    opts = opts |> Enum.into(@defaults)
+
     {scenarios, question} =
       cond do
-        next_question ->
-          {scenarios, next_question}
+        opts.next_question ->
+          {scenarios, opts.next_question}
 
-        next_scenario ->
-          %Scenario{start: start} = Chat.scenario(next_scenario)
+        opts.next_scenario ->
+          %Scenario{start: start} = Chat.scenario(opts.next_scenario)
 
           # If we jump to terminal, then we
           # discard all pending scenarios.
           # Baby, it's the end of the line!
-          if next_scenario == @terminal_scenario do
-            {[next_scenario], start}
+          if opts.next_scenario == @terminal_scenario do
+            {[opts.next_scenario], start}
           else
             [_ | rest] = scenarios
-            {[next_scenario | rest], start}
+            {[opts.next_scenario | rest], start}
           end
 
         true ->
@@ -153,15 +141,34 @@ defmodule Chat.Transitioner do
           end
       end
 
-    scenarios = scenarios |> load_scenario(new_scenario)
+    scenarios =
+      if opts.new_scenario do
+        scenarios ++ [opts.new_scenario]
+      else
+        scenarios
+      end
 
-    {scenarios, question}
+    scenarios =
+      if opts.load_scenarios do
+        opts.new_scenarios |> Enum.reduce(scenarios, &(&2 ++ [&1]))
+      else
+        scenarios
+      end
+
+    {scenarios, question, data}
   end
 
-  defp prepare_comments(data, comments, scenario) do
-    with comments <- comments |> Enum.map(&{&1, scenario}) do
-      data |> Map.put(:comments, comments)
-    end
+  defp put_comments({scenarios, question, data}, comments, current) do
+    data =
+      with comments <- comments |> Enum.map(&{&1, current}) do
+        data |> Map.put(:comments, comments)
+      end
+
+    {scenarios, question, data}
+  end
+
+  defp put_free_text({scenarios, question, data}, answer) do
+    {scenarios, question, data |> Map.put(question, answer)}
   end
 
   defp find_decision(decisions, answer) do
@@ -169,7 +176,4 @@ defmodule Chat.Transitioner do
       decisions |> Enum.find(default, &Util.equal(&1.case, answer))
     end
   end
-
-  defp load_scenario(scenarios, nil), do: scenarios
-  defp load_scenario(scenarios, scenario), do: scenarios ++ [scenario]
 end
