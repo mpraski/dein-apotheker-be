284c9d35141e6f47aa2ed02705f43f9f36d9ed22
mpraski <marcin.praski@live.com>
2020-06-03 18:18:38 +0200

    Dumb mistake

--- apps/domain/config/prod.exs
+++ apps/domain/config/prod.exs
@@ -5,4 +5,4 @@ config :domain, scenario_path: "../../dein-apotheker-scenarios"
 # Configures Elixir's Logger
 config :logger, :console,
   level: :warn,
-  compile_time_purge_level: :info,
+  compile_time_purge_level: :info
